9561f32a951f64980b90b27e5e88f1846dd80ac9
Thinagar <thinagarsivadas@gmail.com>
2021-01-10 21:53:02 +0800

    Updated momentum optimiser

--- NeuralNetwork/optimiser.py
+++ NeuralNetwork/optimiser.py
@@ -14,6 +14,7 @@ class SGD(object):
 
 class SGDM(object):
     """Stochastic Gradient Descent with Momentum
+    Using the purple version in the video
     https://www.youtube.com/watch?app=desktop&v=k8fTYJPd3_I&ab_channel=Deeplearning.ai
     https://towardsdatascience.com/stochastic-gradient-descent-with-momentum-a84097641a5d
     https://medium.com/optimization-algorithms-for-deep-neural-networks/gradient-descent-with-momentum-dce805cd8de8
@@ -27,14 +28,12 @@ class SGDM(object):
         delta_weights = np.dot(activation_prev.T, upstream_grad)
         if hasattr(self, 'delta_weights_current') == False:
             self.delta_weights_current = np.zeros_like(delta_weights)
-        self.delta_weights_current = (self.momentum * self.delta_weights_current) +\
-                                        ((1- self.momentum) * delta_weights)
+        self.delta_weights_current = (self.momentum * self.delta_weights_current) + delta_weights
 
         delta_bias = np.sum(upstream_grad, axis=0, keepdims=True)
         if hasattr(self, 'delta_bias_current') == False:
             self.delta_bias_current = np.zeros_like(delta_bias)
-        self.delta_bias_current = (self.momentum * self.delta_bias_current) +\
-                                        ((1- self.momentum) * delta_bias)
+        self.delta_bias_current = (self.momentum * self.delta_bias_current) + delta_bias
 
         delta_grad = np.dot(upstream_grad, weights.T)
         return self.delta_weights_current, self.delta_bias_current, delta_grad
