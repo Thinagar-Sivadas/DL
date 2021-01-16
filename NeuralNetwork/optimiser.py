b137883dbae9c0e631764124f650013e305026cd
Thinagar <thinagarsivadas@gmail.com>
2021-01-16 22:05:12 +0800

    Updated jupyter notebook

--- NeuralNetwork/optimiser.py
+++ NeuralNetwork/optimiser.py
@@ -14,7 +14,7 @@ class SGD(object):
 
 class SGDM(object):
     """Stochastic Gradient Descent with Momentum
-    Using the purple version in the video
+    Using the purple version in andrew ng video
     https://www.youtube.com/watch?app=desktop&v=k8fTYJPd3_I&ab_channel=Deeplearning.ai
     https://www.youtube.com/watch?v=ewN0vFYFJ7A&ab_channel=NPTEL-NOCIITM
     https://towardsdatascience.com/stochastic-gradient-descent-with-momentum-a84097641a5d
